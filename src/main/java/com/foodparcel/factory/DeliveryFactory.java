c950544adddfd1941db2f72ece5035a01b65a4e9
Craig Angelo <craig.bailey96@gmail.com>
2020-07-05 11:10:22 +0200

    Updated Delivery Factory and Test

--- src/main/java/com/foodparcel/factory/DeliveryFactory.java
+++ src/main/java/com/foodparcel/factory/DeliveryFactory.java
@@ -4,9 +4,15 @@ import com.foodparcel.entity.Delivery;
 
 public class DeliveryFactory {
 
-    public static Delivery getDeliveryAddress(String deliveryAddress) {
+    public static Delivery createDelivery (String deliveryAddress,String deliveryDate, Boolean Delivered) {
+                    Delivery delivery = new Delivery.Builder()
+                    .setDeliveryAddress(deliveryAddress)
+                    .setDeliveryDate(deliveryDate)
+                    .setDelivered(Delivered)
+                    .build();
 
-        return new Delivery.Builder().setDeliveryAddress(deliveryAddress).build();
+
+        return delivery;
 
     }
 }
