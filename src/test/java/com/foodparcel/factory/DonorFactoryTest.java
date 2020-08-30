e9d26e4f9f479be77cb5ead2e4548c4786c7ea33
yusrahsoeker <40632654+yusrahsoeker@users.noreply.github.com>
2020-08-30 21:18:35 +0200

    Update DonorFactoryTest.java

--- src/test/java/com/foodparcel/factory/DonorFactoryTest.java
+++ src/test/java/com/foodparcel/factory/DonorFactoryTest.java
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
+
 package com.foodparcel.factory;
-
-import com.foodparcel.entity.Donor;
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class DonorFactoryTest {
-
-    @Test
-    public void createDonor() {
-        Donor donor = DonorFactory.createDonor("testName", "testSurname", "testEmail",
-                "testPhoneNumber");
-        Assert.assertEquals("testName", donor.getFirstName());
-    }
-=======
-package com.foodparcel.factory;
-
+/**
+ * Author: Yusrah Soeker
+ * Student number: 218150768
+ * */
 import com.foodparcel.entity.Donor;
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,5 +18,4 @@ public class DonorFactoryTest {
                  "testPhoneNumber");
         Assert.assertEquals("testName", donor.getFirstName());
     }
->>>>>>> fa981789147286f0246885eb07d61397839685d1
 }
