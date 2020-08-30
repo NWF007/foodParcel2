1f6053cc3047b384fa25e7acd6473dc94b51d5af
yusrahsoeker <40632654+yusrahsoeker@users.noreply.github.com>
2020-08-30 21:08:43 +0200

    Update DonationFactory.java

--- src/main/java/com/foodparcel/factory/DonationFactory.java
+++ src/main/java/com/foodparcel/factory/DonationFactory.java
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
 package com.foodparcel.factory;
-
+/**
+ * Author: Yusrah Soeker
+ * Student number: 218150768
+ * */
 import com.foodparcel.entity.Donation;
 import com.foodparcel.util.IDGenerator;
 
@@ -16,23 +18,3 @@ public class DonationFactory {
     }
 }
 
-=======
-package com.foodparcel.factory;
-
-import com.foodparcel.entity.Donation;
-import com.foodparcel.util.IDGenerator;
-
-
-public class DonationFactory {
-    public static Donation createDonation(String donationDate, double donateAmount) {
-        String donationId = IDGenerator.generateId();
-        Donation donation = new Donation.Builder()
-                .setDonationId(donationId)
-                .setDonationDate(donationDate)
-                .setDonateAmount(donateAmount)
-                .build();
-        return donation;
-    }
-}
-
->>>>>>> fa981789147286f0246885eb07d61397839685d1
