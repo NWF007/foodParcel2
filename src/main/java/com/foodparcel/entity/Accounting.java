9b3ffd5e69e095ece9ea1e58ddf76f6414ff937c
yusrahsoeker <40632654+yusrahsoeker@users.noreply.github.com>
2020-08-30 20:59:03 +0200

    Update Accounting.java

--- src/main/java/com/foodparcel/entity/Accounting.java
+++ src/main/java/com/foodparcel/entity/Accounting.java
@@ -7,10 +7,7 @@ package com.foodparcel.entity;
  */
 
 public class Accounting {
-<<<<<<< HEAD
     private String statementId;
-=======
->>>>>>> fa981789147286f0246885eb07d61397839685d1
     private double income;
     private double expense;
     private double budget;
@@ -18,10 +15,7 @@ public class Accounting {
     private int expenseTransactionId;
 
     private Accounting(Builder builder){
-<<<<<<< HEAD
         this.statementId = builder.statementId;
-=======
->>>>>>> fa981789147286f0246885eb07d61397839685d1
         this.income = builder.income;
         this.expense = builder.expense;
         this.budget = builder.budget;
@@ -29,13 +23,10 @@ public class Accounting {
         this.expenseTransactionId = builder.expenseTransactionId;
     }
 
-<<<<<<< HEAD
     public String getStatementId() {
         return statementId;
     }
 
-=======
->>>>>>> fa981789147286f0246885eb07d61397839685d1
     public double getIncome() {
         return income;
     }
@@ -58,14 +49,9 @@ public class Accounting {
 
     @Override
     public String toString() {
-<<<<<<< HEAD
         return "Builder{" +
                 " statementId=" + statementId +
                 ", income=" + income +
-=======
-        return "Accounting{" +
-                "income=" + income +
->>>>>>> fa981789147286f0246885eb07d61397839685d1
                 ", expense=" + expense +
                 ", budget=" + budget +
                 ", incomeTransactionId=" + incomeTransactionId +
@@ -76,15 +62,12 @@ public class Accounting {
     public static class Builder{
         private double income, expense, budget;
         private int incomeTransactionId, expenseTransactionId;
-<<<<<<< HEAD
         private String statementId;
 
         public Builder setStatementId(String statementId){
             this.statementId = statementId;
             return this;
         }
-=======
->>>>>>> fa981789147286f0246885eb07d61397839685d1
 
         public Builder setIncome(double income){
             this.income = income;
@@ -112,10 +95,7 @@ public class Accounting {
         }
 
         public Builder copy(Accounting accounting){
-<<<<<<< HEAD
             this.statementId = accounting.statementId;
-=======
->>>>>>> fa981789147286f0246885eb07d61397839685d1
             this.income = accounting.income;
             this.expense = accounting.expense;
             this.budget = accounting.budget;
