3e071f33b7e633db247544eefd027940404e358f
Craig Angelo <craig.bailey96@gmail.com>
2020-09-20 20:00:43 +0200

    FoodParcelApp added

---
+++ src/main/java/com/foodparcel/FoodParcelApplication.java
@@ -0,0 +1,14 @@
+package com.foodparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FoodParcelApplication {
+
+    public static void main(String [] args){
+
+        SpringApplication.run(FoodParcelApplication.class, args);
+    }
+}
